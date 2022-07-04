64711ac4b0b04465ac2892a109545c6fa55ebeda
coderbasket <coderbasketcontact@gmail.com>
2022-07-04 17:53:35 +0800

    update maintainance

--- Lib/Shared/Helpers/SafeHelper.cs
+++ Lib/Shared/Helpers/SafeHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blazor_App.Shared.Extensions;
+using Blazor_App.Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +10,31 @@ namespace Blazor_App.Shared
 {
     public class SafeHelper
     {
-        public static List<string> GetContainsSafeImageUrls()
+        static List<string> list = null;
+        public async static Task<List<string>> GetContainsSafeImageUrlsAsync()
         {
-            var list = new List<string>();
-            list.Add("https://github.com/");
-            list.Add("githubusercontent");
-            list.Add("https://user-images.githubusercontent");
-            list.Add("https://raw.githubusercontent.com");
-            list.Add("amazonaws");
-            list.Add("uno-website-assets");
-            list.Add("https://camo.githubusercontent");
-            list.Add("visualstudiomagazine.com");
-            list.Add("medium.com");
-            list.Add("microsoft.com");
-            list.Add("devblogs.microsoft.com");
-            list.Add("telerik.com");
-            list.Add("miro.medium.com");
-            list.Add("https://play-lh.googleusercontent.com/");
+            if (list != null && list.Count > 0)
+                return list;
+            await Task.Delay(1);
+#if DEBUG
+            var stream = RepoHelper.GetResourceStreamAsync(typeof(SafeHelper), "Blazor_App.Shared.Data.safe_urls.json");
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string text = reader.ReadToEnd();
+                if (text.IsValidString())
+                {
+                    list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<string>>(text);
+                }
+            }
+#else
+            string url = "https://github.com/coderbasket/coderbasket.github.io/blob/development/Lib/Shared/Data/safe_urls.json";
+            var txt = await CookUpServices.DownloadstringAsync(url);
+            if (txt.IsValidString())
+            {
+                list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<string>>(txt);
+            }
+           
+#endif
             return list;
         }
     }
