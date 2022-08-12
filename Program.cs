25d6e8da009b3474e257f2084e053e70d53ef500
coderbasket <coderbasketcontact@gmail.com>
2022-08-12 23:56:41 +0800

    ok

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredModal();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<UserSettingsProvider>();
+//SiteInfo.GetHeader();
 DataServiceProvider.LoadDeveloperTools();
 var items = await DataServiceProvider.GetItemsAsync();
 
